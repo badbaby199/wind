e1fd6087f05dc292e8f61f146c88ceef33f92c04
badbaby199 <95273688+badbaby199@users.noreply.github.com>
2023-08-08 14:53:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 设置各变量，WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-WSPATH=${WSPATH:-'erwwrt'}
-UUID=${UUID:-'0ab5a971-ba17-4a8a-93c6-1a23a68c0338'}
+WSPATH=${WSPATH:-'vxby3q'}
+UUID=${UUID:-'3a194479-3490-4356-96db-00717241a27e'}
 
 # 安装系统依赖
 check_dependencies() {
